dc2338725d00c9243033737ec9e4e382bca93ecb
BenWiggenhorn <ben.wiggenhorn@AstroAITrading.com>
2025-01-27 23:46:01 -0600

    betmgm mostly finished, debugging left

--- scrapeBetMGM.py
+++ scrapeBetMGM.py
@@ -6,7 +6,7 @@ import time
 #TODO: work on fixing the live maybe? not sure how it works with live games
 
 
-def get_date(date):
+def get_date( date):
     if "Tomorrow" in date:
     #might need to bring back
     # tomorrow = dt.datetime.now() + dt.timedelta(days=1)
@@ -14,7 +14,11 @@ def get_date(date):
     elif "Today" in date:
         date = dt.datetime.now().strftime("%b %#d, %Y")
     else:
-        parsed_date = dt.datetime.strptime(date, '%m/%d/%y • %I:%M %p')
+        #needs because sometimes the • doesnt load 
+        if "•" in date:
+            parsed_date = dt.datetime.strptime(date, '%m/%d/%y • %I:%M %p')
+        else:
+            parsed_date = dt.datetime.strptime(date, '%m/%d/%y %I:%M %p')
         date = parsed_date.strftime("%b %#d, %Y")
 
     return date
@@ -24,49 +28,49 @@ def get_sports(page):
     sports = {} 
     #get left widget items
     top_widget = page.locator("#sports-nav a")
-    top_widget_count = top_widget.count()
     try:
         try:
             popup = page.locator('svg[title="theme-ex"]')
-            popup.wait_for(state='visible', timeout=6000)
+            popup.wait_for(state='visible', timeout=4000)
             if popup.is_visible():
                 popup.click()
         except Exception as e:
             pass
-
-        sports["NFL"] = top_widget.locator("text=NFL")
-        # sports["NBA"] = top_widget.locator("text=NBA", exact=True)   
-        # sports["NCAAB"] = top_widget.locator("text=NCAAB")    
-        # sports["NHL"] = top_widget.locator("text=NHL")
+        
+        sports["NFL"] = top_widget.locator(":text-is('NFL')")
+        sports["NBA"] = top_widget.locator(":text-is('NBA')")
+        # sports["NCAAM"] = top_widget.locator(":text-is('NCAAM')")    
+        # sports["NHL"] = top_widget.locator(":text-is('NHL')")
     
     except Exception as e:
         print("Error getting  sports:", e)
 
     return sports
-#TODO: not updated
+
+#TODO: check when theres a +100, if it says that or just says even
 def get_odds_for_single_team(team_name, spread, total, moneyline):
-    
+
+    #get team info
     team_info = {}
-    team_info["team"] = team.locator(".text-style-s-medium.text-primary.text-primary").text_content()
-    odds = team.get_by_test_id("Add Bet Selections")
-    num_odds = odds.count()
-    for i in range(num_odds):
-        odds_text = odds.nth(i).text_content()
-        if "O" in odds_text:
-            matches = re.findall(r"-?\d+\.\d+|-?\d+", odds_text)
-            team_info["over"] = {"value": matches[0], "odds": matches[1] if len(matches) > 1 else "+100"}
-        elif "U" in odds_text:
-            matches = re.findall(r"-?\d+\.\d+|-?\d+", odds_text)
-            team_info["under"] = {"value": matches[0], "odds": matches[1] if len(matches) > 1 else "+100"}
-        elif "." in odds_text:
-            matches = re.findall(r"[+-]?\d+\.\d+|[+-]?\d+", odds_text)
-            team_info["spread"] = {"value": matches[0], "odds": matches[1] if len(matches) > 1 else "+100"}
-        else:
-            team_info["moneyline"] = "+100" if odds_text == "Even" else odds_text 
+    team_info["team"] = team_name.text_content()
+
+    #get spread odds
+    spread_odds = spread.text_content()
+    matches = re.findall(r"[+-]?\d+\.\d+|[+-]?\d+", spread_odds)
+    team_info["spread"] = {"value": matches[0], "odds": matches[1] }
+
+    #get over/under odds
+    total_odds = total.text_content()
+    over_under = "over" if "O" in total_odds else "under"
+    matches = re.findall(r"-?\d+\.\d+|-?\d+", total_odds)
+    team_info[over_under] = {"value": matches[0], "odds": matches[1]}
+
+    #might need to edit later, not sure what they do in case of tie
+    team_info["moneyline"] = moneyline.text_content()
     return team_info
-def get_odds_for_single_game(game):
+
+def get_odds_for_single_game(page, game):
     game_odds = {}
-    print("in odds for single game")
     try:
         #get team name
         team_name = game.locator('.participant-container')
@@ -80,6 +84,7 @@ def get_odds_for_single_game(game):
         money_info = odds_info.nth(2).locator('ms-option')
         #get date text and format
         date = get_date(game.locator('ms-event-timer').text_content())
+        
         game_odds["date"] = date
         game_odds["away"] = get_odds_for_single_team(team_name.nth(0), spread_info.nth(0), total_info.nth(0), money_info.nth(0))
         game_odds["home"] = get_odds_for_single_team(team_name.nth(1), spread_info.nth(1), total_info.nth(1), money_info.nth(1))
@@ -89,24 +94,23 @@ def get_odds_for_single_game(game):
         game_odds.get("home").pop("under", "none found")
     except Exception as e:
         print("error getting odds for single game", e)
-        print(game.text_content())
     return game_odds
 
 def get_odds(page, sports, p):
     odds = {}
     for (sport, button) in sports.items():
+
         games = []
         button.click()
+        # page.pause()
         try:
             #grid with games
+            page.wait_for_selector("ms-event-group", timeout=4000)
             game_grid = page.locator("ms-event-group")
-            #TODO: might cause slight error, six pack might mean all 6 odds(ML spread total) so ones without might be diff
-            #get all of the boxes that have represent games
             game_list = game_grid.locator("ms-six-pack-event")
             game_count = game_list.count()
             for i in range(game_count):
-                print("in loop for selecting games")
-                games.append(get_odds_for_single_game(game_list.nth(i)))
+                games.append(get_odds_for_single_game(page, game_list.nth(i)))
             odds[sport] = games    
         except Exception as e:
             print("Most likely no games found", e)
@@ -118,7 +122,8 @@ def main():
     with sync_playwright() as p:
 
         browser = p.chromium.launch(headless=False)  
-        page = browser.new_page()
+        context = browser.new_context(permissions=[])
+        page = context.new_page()
         
         # Navigate to the target URL
         page.goto("https://sports.az.betmgm.com/en/sports")
@@ -126,7 +131,7 @@ def main():
 
         sports = get_sports(page)
         odds =  get_odds(page, sports, p)
-        # print(odds)
+        print(odds)
         browser.close()
         p.stop()
 
